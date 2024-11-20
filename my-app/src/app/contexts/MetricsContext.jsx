b0a1e8d843e7f6e29a2158aa710cd0a996cdbf6f
Lisa Saltzberg <lisasaltzberg2025@u.northwestern.edu>
2024-11-20 12:03:22 -0600

    metrics context completed

---
+++ my-app/src/app/contexts/MetricsContext.jsx
@@ -0,0 +1,26 @@
+"use client";
+import {
+    createContext,
+    useContext,
+    useState,
+  } from 'react';
+
+  
+  const MetricsContext = createContext({
+    recipes:{},
+    setMetrics: () => {},
+  });
+  
+  export const useMetricsContext = () => useContext(MetricsContext);
+  
+  export default function MetricsContextProvider({
+    children,
+  }) {
+    const [metrics, setMetrics] = useState({health: 0, happiness: 0, satiation:0, cleanliness: 0});
+  
+    return (
+      <MetricsContext.Provider value={{ metrics, setMetrics}}>
+        {children}
+      </MetricsContext.Provider>
+    );
+  }
