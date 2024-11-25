697675c59b33ec96a92586dfed86aa5d31c02b43
Young-Yu-Shuyang <shuyany6@uci.edu>
2024-11-24 18:19:42 -0600

    implement feature of updating pet basic information
    add a feature that basic information ( name, weight, health, happiness, cleanliness, satiation) could get updated based on specific factors.

---
+++ my-app/src/app/basic_info/pet_info.js
@@ -0,0 +1,43 @@
+import { useMetricsContext } from "../contexts//MetricsContext";
+
+const { metrics, setMetrics } = useMetricsContext();
+
+
+export function updatePetAge(time) {
+    const currentTime = new Date();
+    const hours = Math.floor((currentTime - new Date(time)) / (1000 * 60 * 60)); // the age in hours
+    return  hours;
+}
+
+export function updatePetWeight(weight, time)  {
+    const hours = Math.floor((new Date() - new Date(time)) / (1000 * 60 * 60));
+    const weightChange = metrics.health < 60
+    ? -0.1 * hours // lose weight if it is unhealthy based on hours
+    : 0.05 * hours; // gain weight if it is healthy
+    return weight + weightChange;
+};
+
+export function updateMetricsFunction() {
+    const updateMetrics = (time) => {
+        const hours = Math.floor((new Date() - new Date(time)) / (1000 * 60 * 60));
+
+        setMetrics((prev) => {
+            const newHappiness = metrics.happiness - hoursElapsed * 2;
+            const newSatiation = pet.satiation - hoursElapsed * 3;
+            const newCleanliness = pet.cleanliness - hoursElapsed * 1;
+    
+            const newHealth = Math.round(
+            newHappiness * 0.4 + newSatiation * 0.4 + newCleanliness * 0.2
+            );
+            return {
+                ...prev,
+                health :  newHealth,
+                happiness : newHappiness,
+                satiation : newSatiation,
+                cleanliness : newCleanliness
+            };
+        });
+    };
+    return {updateMetrics};
+
+};
