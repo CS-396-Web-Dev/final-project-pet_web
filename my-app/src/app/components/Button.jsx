e495fc9410c7107935825951af26f440493ab99f
Lisa Saltzberg <lisasaltzberg2025@u.northwestern.edu>
2024-11-25 11:13:58 -0600

    creating buttons component and adding it to page.js

---
+++ my-app/src/app/components/Button.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { useMetricsContext} from '../contexts/MetricsContext';
+
+export default function Buttons({buttonType}) {
+    const {metrics, setMetrics} = useMetricsContext();
+    console.log(metrics)
+    console.log(buttonType)
+    let buttonName;
+    buttonName = {happiness:'Play with Me!', health: 'Heal Me!', cleanliness:'Clean Me!', satiation: 'Feed Me!' };
+
+    return(
+
+        <button
+            id = {`${buttonType}-button`}
+            className = "bg-purple-800 hover:bg-purple-950 text-white py-2 px-4 rounded-full mr-5"
+            onClick={(e) => {
+            e.preventDefault();
+
+            setMetrics((prev) => {
+              const dictCopy = prev;
+              if (buttonType != 'health'){
+                if (dictCopy[buttonType] <= 80){
+                    dictCopy[buttonType] += 20;
+                  }
+                  else{
+                    dictCopy[buttonType] = 100;
+                  }
+    
+                  if (dictCopy.health <= 95){
+                    dictCopy.health += 5;
+                  }
+                  else{
+                    dictCopy.health = 5;
+                  }
+              }else{
+                if (dictCopy.health <= 85){
+                    dictCopy.health += 15;
+                  }
+                  else{
+                    dictCopy.health = 100;
+                  }
+              }
+              
+              
+              return dictCopy;
+  
+            })
+
+            console.log(metrics)
+          }}>
+            {buttonName[buttonType]}
+            </button>
+
+    )
+
+
+}
