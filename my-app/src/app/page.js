9eb399e3b4b19223fe07ab632cd1902653df5868
YangchichengShen <78356476+YangchichengShen@users.noreply.github.com>
2024-11-26 13:10:45 -0600

    🍱 Add images of pikachu and edit button

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -4,7 +4,6 @@ import Buttons from "./components/Buttons"
 export default function Home() {
   return (
     <div>
-      <Bar value={90} label="Test"></Bar>
       <Buttons></Buttons>
     </div>
   );
