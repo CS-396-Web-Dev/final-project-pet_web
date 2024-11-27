6e40c9fc8d4583d6567e030258de3a90cb87ba44
Lisa Saltzberg <lisasaltzberg2025@u.northwestern.edu>
2024-11-26 21:45:52 -0600

    adding reset functionality

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,13 +2,15 @@
 import Bars from "./components/Bars"
 import { PetName, PetAge, PetWeight } from "@/app/components/basicInfoText";
 import Buttons from "./components/Buttons"
-import {useInfoContext} from './contexts/InfoContext';
+import Reset from "./components/Reset"
+import {useInfoContext} from "./contexts/InfoContext";
 
 export default function Home() {
   const {info} = useInfoContext();
 
   return (
     <div>
+      <Reset />
       <PetName/>
       <PetAge ageInHour={info.age} growthStage={info.stage}/>
       <PetWeight weight={info.weight}/>
