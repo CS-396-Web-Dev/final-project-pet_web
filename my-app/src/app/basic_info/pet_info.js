93bd005759128ac9083be051b5122dea7f2b905b
Young-Yu-Shuyang <shuyany6@uci.edu>
2024-11-30 15:05:33 -0500

    fixed pet metrics update bugs
    Fixed bug in pet health dropping abnormally fast. Make the displayed metrics values rounded to 1 decimal places

--- my-app/src/app/basic_info/pet_info.js
+++ my-app/src/app/basic_info/pet_info.js
@@ -1,15 +1,15 @@
 export function updatePetAge(time) {
     const currentTime = new Date();
     const hours  = (currentTime - new Date(time)) / (1000 * 60 * 60); // the age in half hours
-    return  hours;
+    return  hours.toFixed(2);
 }
 
 export function updatePetWeight(weight, time,health)  {
     const hours = (new Date() - new Date(time)) / (1000* 60 * 60);
     const weightChange = health < 60
     ? -0.1 * hours // lose weight if it is unhealthy based on hours
-    : 0.05 * hours; // gain weight if it is healthy
-    return Math.max(0,weight + weightChange);
+    : 0.01 * hours; // gain weight if it is healthy
+    return Math.max(0,(weight + weightChange).toFixed(1));
 };
 
 export function updateMetricsFunction(setMetrics, time) {
@@ -17,13 +17,13 @@ export function updateMetricsFunction(setMetrics, time) {
     const hours = (new Date() - new Date(time)) / (1000* 60 * 60);
 
     setMetrics((prev) => {
-        const newHappiness = Math.max(0,prev.happiness - hours * 2);
-        const newSatiation = Math.max(0,prev.satiation - hours * 3);
-        const newCleanliness = Math.max(0,prev.cleanliness - hours * 1);
+        const newHappiness = Math.max(0,(prev.happiness - hours * 0.5).toFixed(1));
+        const newSatiation = Math.max(0,(prev.satiation - hours * 0.5).toFixed(1));
+        const newCleanliness = Math.max(0,(prev.cleanliness - hours * 0.5).toFixed(1));
 
-        const newHealth = Math.round(
-        newHappiness * 0.4 + newSatiation * 0.4 + newCleanliness * 0.2
-        );
+        const newHealth = Math.max(0,Math.round((prev.health - hours * 1)*0.99 +
+        (newHappiness * 0.4 + newSatiation * 0.4 + newCleanliness * 0.2) * 0.01
+        ));
         return {
             ...prev,
             health :  newHealth,
