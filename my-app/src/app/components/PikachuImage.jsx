ddb5e2c66b7180b46121c94ae7ed86f2acc90f70
YangchichengShen <78356476+YangchichengShen@users.noreply.github.com>
2024-12-01 12:38:00 -0600

    Add 3 different image options for Pikachu. One for death, one for unhappy, one for happy. Change images accordingly.

--- my-app/src/app/components/PikachuImage.jsx
+++ my-app/src/app/components/PikachuImage.jsx
@@ -6,7 +6,14 @@ export default function PikachuImage() {
     console.log(metrics.health)
     var srcLink = "/assets/pikachu.gif"
     // Logic for choosing the image
-
+    // not happy pikachu
+    if (metrics.happiness <= 30) {
+        srcLink = "/assets/sleep_pikachu.gif"
+    }
+    // death stage
+    if (metrics.health <= 0 || metrics.happiness <= 0 || metrics.satiation <=0 || metrics.cleanliness <= 0 ){
+        srcLink = "/assets/gravestone.jpg"
+    } 
     return (
         <img src={srcLink} alt="Pikachu" className="w-48 h-auto"></img>
     )
